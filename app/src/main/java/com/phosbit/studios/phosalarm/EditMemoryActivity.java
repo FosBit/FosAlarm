cb5ec3fe5e3fbf0799e322ce3e7872a81ce6cbff
Aaron Chen <aaronjc12@gmail.com>
2017-12-09 16:39:47 -0800

    Added functionality to Edit/Delete buttons in Memory Bank

---
+++ app/src/main/java/com/phosbit/studios/phosalarm/EditMemoryActivity.java
@@ -0,0 +1,13 @@
+package com.phosbit.studios.phosalarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditMemoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_memory);
+    }
+}
--- app/src/main/java/com/phosbit/studios/phosalarm/MemoryBankRVAdapter.java
+++ app/src/main/java/com/phosbit/studios/phosalarm/MemoryBankRVAdapter.java
@@ -1,11 +1,15 @@
 package com.phosbit.studios.phosalarm;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.support.design.widget.Snackbar;
 
 import java.util.List;
 
@@ -18,20 +22,25 @@ public class MemoryBankRVAdapter extends RecyclerView.Adapter< MemoryBankRVAdapt
 
     public static class MemoriesViewHolder extends RecyclerView.ViewHolder
     {
-
         // Provide a reference to the views for each data item
         // Complex data items may need more than one view per item, and
         // you provide access to all the views for a data item in a view holder
         CardView cv;
         TextView memoryName;
         TextView memoryDesc;
+        Button memoryEdit;
+        Button memoryDelete;
+
 
         MemoriesViewHolder( View itemView )
         {
             super( itemView );
+            // Find views to put in the references
             cv = ( CardView ) itemView.findViewById( R.id.memory_cardview );
             memoryName = ( TextView ) itemView.findViewById( R.id.memory_title );
             memoryDesc = ( TextView ) itemView.findViewById( R.id.memory_description );
+            memoryEdit = ( Button ) itemView.findViewById( R.id.memory_edit_button );
+            memoryDelete = ( Button ) itemView.findViewById( R.id.memory_delete_button );
         }
     }
 
@@ -60,10 +69,45 @@ public class MemoryBankRVAdapter extends RecyclerView.Adapter< MemoryBankRVAdapt
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder( MemoriesViewHolder personViewHolder, int i )
+    public void onBindViewHolder( MemoriesViewHolder holder, final int i )
     {
-        personViewHolder.memoryName.setText( memories.get( i ).getTitle() );
-        personViewHolder.memoryDesc.setText( memories.get( i ).getContent() );
+        holder.memoryName.setText( memories.get( i ).getTitle() );
+        holder.memoryDesc.setText( memories.get( i ).getContent() );
+
+        // Set a click listener for memory edit
+        holder.memoryEdit.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v )
+            {
+                // Start edit activity
+                Intent intent = new Intent( v.getContext(), EditMemoryActivity.class );
+                v.getContext().startActivity( intent );
+            }
+        });
+
+        // Same for memory delete
+        holder.memoryDelete.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v )
+            {
+                // Get the clicked item label
+                String memoryTitle = memories.get( i ).getTitle();
+                // Remove the item on delete button click
+                memories.remove( i );
+                // Notify any registered observers that the item previously located at position
+                // has been removed from the data set. The items previously located at and
+                // after position may now be found at oldPosition - 1.
+                notifyItemRemoved( i);
+                // Notify any registered observers that the itemCount items starting at
+                // position positionStart have changed.
+                notifyItemRangeChanged( i, memories.size());
+
+                // Show the removed item title
+                Snackbar.make( v, "Removed: " + memoryTitle, Snackbar.LENGTH_LONG ).show();
+            }
+        });
     }
 
     // Return the size of your dataset (invoked by the layout manager)
