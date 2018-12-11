1415ff395c32554c924b676cf4f542c062c4d205
Aaron <aaronjc12@gmail.com>
2018-12-11 13:47:05 -0800

    Added ability for users to fill in random words in the memory
    - Instead of having to type out the whole memory, if the memory is long
    then we should try to have the user just fill in certain words

--- app/src/main/java/com/fosbit/studios/fosalarm/ui/AlarmActivity.java
+++ app/src/main/java/com/fosbit/studios/fosalarm/ui/AlarmActivity.java
@@ -11,70 +11,185 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Pair;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.fosbit.studios.fosalarm.R;
 import com.fosbit.studios.fosalarm.db.Alarm;
 import com.fosbit.studios.fosalarm.db.FosViewModel;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class AlarmActivity extends AppCompatActivity {
     TextView memoryTitle;
-    EditText memoryMessage;
+    LinearLayout messageLayout;
     Button dismissAlarm;
     Ringtone ringtone;
-    String memoryValidation;
+    String fullMemoryMessage;
+    String[] memoryMessage;
     Alarm alarm;
+    HashMap<Integer, Pair<String, Boolean>> wordsFillMap;
 
     private FosViewModel mFosViewModel;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alarm);
+        getWindow().addFlags( WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED );
+        getWindow().addFlags( WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD );
+        getWindow().addFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
+        getWindow().addFlags( WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON ) ;
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_alarm );
 
-        mFosViewModel = ViewModelProviders.of(this).get(FosViewModel.class);
+        mFosViewModel = ViewModelProviders.of(this ).get( FosViewModel.class );
+        wordsFillMap = new HashMap<Integer, Pair<String, Boolean>>();
 
-        memoryTitle = (TextView) findViewById(R.id.memory_title_textview);
-        memoryMessage = (EditText) findViewById(R.id.memory_message);
-        dismissAlarm = (Button) findViewById(R.id.dismiss_alarm_button);
+        memoryTitle = findViewById(R.id.memory_title_textview);
+        messageLayout = findViewById(R.id.memory_message_layout);
+        dismissAlarm = findViewById(R.id.dismiss_alarm_button);
+        dismissAlarm.setClickable( false );
 
         Bundle bundle = getIntent().getExtras();
-        memoryTitle.setText(bundle.getString("TITLE"));
-        memoryValidation = bundle.getString("MESSAGE");
-        alarm = new Alarm(bundle.getString("ALARMID"), bundle.getLong("TIME"),
-                false, bundle.getString("MEMORYID"));
-        if ( !memoryValidation.isEmpty() ) {
-            dismissAlarm.setClickable( false );
-        } else {
-            memoryMessage.setVisibility( View.GONE );
-        }
-
-
-        memoryMessage.addTextChangedListener(new TextWatcher() {
-            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override public void afterTextChanged(Editable s) {
-                String enteredText = s.toString();
-                //do validation here
-                if ( enteredText.equals( memoryValidation ) ) {
-                    dismissAlarm.setClickable( true );
+        memoryTitle.setText( bundle.getString("TITLE" ) );
+        fullMemoryMessage = bundle.getString("MESSAGE" );
+        memoryMessage = fullMemoryMessage.split(" " );
+        alarm = new Alarm( bundle.getString("ALARMID" ), bundle.getLong("TIME" ),
+                false, bundle.getString("MEMORYID" ) );
+        if ( memoryMessage.length > 0 ) {
+            List<LinearLayout> messageLines = new ArrayList<>();
+            // Split string by spaces
+            // If omitting words means there's more than 5 words left, then we can
+            // replace words with a blank space for user to enter in the correct word
+            if ( ( memoryMessage.length - 5 >= 5 ) ) {
+                while( wordsFillMap.size() < 5 ) {
+                    // (* max + 1 ) usually + 1 but the length - 1 cancels out
+                    int randnum = (int) ( Math.random() * memoryMessage.length );
+                    if ( !wordsFillMap.containsKey( randnum ) ) {
+                        wordsFillMap.put( randnum, new Pair<>( memoryMessage[randnum], false ) );
+                    }
                 }
+
+                int i = 0;
+                while( i < memoryMessage.length ) {
+                    int layoutWidth = messageLayout.getLayoutParams().width;
+                    LinearLayout line = new LinearLayout( this );
+                    line.setOrientation( LinearLayout.HORIZONTAL );
+                    while ( ( layoutWidth > 0 ) && ( i < memoryMessage.length ) ) {
+                        if ( !wordsFillMap.containsKey( i ) ) {
+                            TextView tv = new TextView( this );
+                            int width = (int)( memoryMessage[i].length() *  tv.getTextSize() );
+                            layoutWidth = layoutWidth - width;
+                            if ( layoutWidth <= 0 ) {
+                                break;
+                            }
+                            tv.setLayoutParams(new LinearLayout.LayoutParams(
+                                    width,
+                                    ViewGroup.LayoutParams.WRAP_CONTENT));
+                            tv.setText( memoryMessage[i] );
+                            line.addView( tv );
+                            i++;
+                        } else {
+                            final int mapKey = i;
+                            EditText et = new EditText( this ); // Pass it an Activity or Context
+                            int width = (int)( memoryMessage[i].length() *  et.getTextSize() );
+                            layoutWidth = layoutWidth - width;
+                            if ( layoutWidth <= 0 ) {
+                                break;
+                            }
+                            et.setLayoutParams( new LinearLayout.LayoutParams(
+                                    width,
+                                    LinearLayout.LayoutParams.WRAP_CONTENT) );
+                            et.addTextChangedListener(new TextWatcher() {
+                                @Override
+                                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                                }
+
+                                @Override
+                                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                                }
+
+                                @Override
+                                public void afterTextChanged(Editable editable) {
+                                    String enteredText = editable.toString();
+                                    String actualText  = wordsFillMap.get( mapKey ).first;
+                                    //do validation here
+                                    if ( enteredText.equals( actualText ) ) {
+                                        if ( !wordsFillMap.get( mapKey ).second ) {
+                                            wordsFillMap.remove(mapKey);
+                                            wordsFillMap.put(mapKey, new Pair<>(actualText, true));
+                                        }
+                                    } else {
+                                        if ( wordsFillMap.get( mapKey ).second ) {
+                                            wordsFillMap.remove(mapKey);
+                                            wordsFillMap.put(mapKey, new Pair<>(actualText, false));
+                                        }
+                                    }
+
+                                    // If there is even one input that is not valid,
+                                    // then do not let user dismiss the alarm.
+                                    for ( Pair< String, Boolean> pair : wordsFillMap.values() )  {
+                                        if ( !pair.second ) {
+                                            dismissAlarm.setClickable( false );
+                                            break;
+                                        } else {
+                                            dismissAlarm.setClickable( true );
+                                        }
+                                    }
+                                }
+                            });
+                            line.addView( et );
+                            i++;
+                        }
+                    }
+                    messageLines.add( line );
+                }
+
+                for ( LinearLayout ll : messageLines ) {
+                    messageLayout.addView( ll );
+                }
+            } else {
+                EditText et = new EditText( this ); // Pass it an Activity or Context
+                et.setLayoutParams( new LinearLayout.LayoutParams(
+                        messageLayout.getLayoutParams().width,
+                        LinearLayout.LayoutParams.WRAP_CONTENT) );
+                et.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+                        String enteredText = editable.toString();
+                        //do validation here
+                        if ( enteredText.equals( fullMemoryMessage ) ) {
+                            dismissAlarm.setClickable( true );
+                        } else {
+                            dismissAlarm.setClickable( true );
+                        }
+                    }
+                });
+
+                messageLayout.addView( et );
             }
-        });
+        } else {
+            dismissAlarm.setClickable( true );
+        }
 
         // Gets default alarm to ring when alarm is sounded
         Uri alarmUri = RingtoneManager.getDefaultUri( RingtoneManager.TYPE_ALARM );
